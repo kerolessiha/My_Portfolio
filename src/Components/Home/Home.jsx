49c3054443e3476b306f0989477af2311990ee2d
Keroles Siha <kerolessiha999@gmail.com>
2025-04-22 15:07:11 +0200

    تحديث رابط CV في الواجهة

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -52,7 +52,7 @@ export default function Home() {
 
                 {/* Download CV Button */}
                 <a
-                  href="/Keroles-Siha-Saad.pdf"
+                  href="/public/Keroles-Siha-Saad-CV.pdf"
                   download="Keroles-Siha-Saad.pdf"
                   className="px-6 py-3 bg-indigo-600 text-white rounded-full font-semibold transition-transform duration-300 hover:scale-105 hover:shadow-lg"
                 >
